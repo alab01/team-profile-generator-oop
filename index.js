333e9a3804b227bc833ab59e495c444f9f00d873
Alan Abed <alanabed@Alans-MacBook-Air.local>
2022-06-24 16:35:24 -0700

    added index.js file content with questions and functions for prompts

--- index.js
+++ index.js
@@ -1,13 +1,116 @@
 const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-const employee1 = new Employee('Ron Burgundy', 'abc123', 'test@email.com');
+const teamMemberArray = [];
 
-const name1 = employee1.getName();
-const id1 = employee1.getId();
-const email1 = employee1.getEmail();
-const role1 = employee1.getRole();
 
-console.log(name1);
-console.log(id1);
-console.log(email1);
-console.log(role1);
+const managerQuestions = [
+    {
+      type: 'input',
+      message: "What is your manager's name?",
+      name: 'managerName',
+    },
+    {
+      type: 'input',
+      message: "What is your manager's ID?",
+      name: 'managerId',
+    },
+    {
+      type: 'input',
+      message: "What is your manager's email address?",
+      name: 'managerEmail',
+    },
+    {
+      type: 'input',
+      message: "What is your manager's office number?",
+      name: 'managerOffice',
+    },
+]
+
+function mangagerPrompt() {
+    return inquirer.prompt (managerQuestions).then((data) => {
+        console.log(data);
+        const managerName = data.managerName;
+        const managerId = data.managerId;
+        const managerEmail = data.managerEmail;
+        const managerOffice = data.managerOffice;
+        const manager = new Manager(managerName, managerId, managerEmail, managerOffice);
+        teamMemberArray.push(manager);
+    });
+}
+
+
+const employeeQuestions = [
+    {
+        type: 'list',
+        message: 'What is your employees role?',
+        name: 'employeeRole',
+        choices: ['Engineer', 'Intern'],
+    },
+    {
+        type: 'input',
+        message: "What is the employees name?",
+        name: 'employeeName',
+    },
+    {
+        type: 'input',
+        message: "What is the employees ID?",
+        name: 'employeeId',
+    },
+    {
+        type: 'input',
+        message: "What is your the employees email address?",
+        name: 'employeeEmail',
+    },
+    {
+        type: 'input',
+        message: "What is your employees GitHub username?",
+        name: 'employeeGitHub',
+    },
+    {
+        type: 'input',
+        message: "What is your employee school?",
+        name: 'employeeSchool',
+    },
+
+]
+
+function employeePrompt() {
+    return inquirer.prompt (employeeQuestions).then((data) => {
+        console.log(data);
+        const employeeRole = data.employeeRole;
+        const employeeName = data.employeeName;
+        const employeeId = data.employeeId;
+        const employeeEmail = data.employeeEmail;
+        const employeeGitHub = data.employeeGitHub;
+        const employeeSchool = data.employeeSchool;
+    
+        if (employeeRole === "Intern") {
+            const intern = new Intern(employeeName, employeeId, employeeEmail, employeeSchool);
+            teamMemberArray.push(intern);
+        } else {
+            const engineer = new Engineer(employeeName, employeeId, employeeEmail, employeeGitHub);
+            teamMemberArray.push(engineer);
+        }
+    
+    });
+}
+
+mangagerPrompt().then(employeePrompt);
+
+inquirer.prompt(mangagerPrompt().then(employeePrompt)).then((data) => {
+    console.log(data);
+    const htmlContent = generateHtml(data);
+    console.log(htmlContent);
+
+    fs.writeFile('index.html', htmlContent, (err) =>
+        err ? console.log(err) : console.log('Success!')
+    );
+});
+
+
+console.log(teamMemberArray)
